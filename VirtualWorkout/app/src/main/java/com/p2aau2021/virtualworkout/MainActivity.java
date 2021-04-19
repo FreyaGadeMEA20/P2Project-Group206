72fe0c8d50e7cd47bb8347add373ecc8d1b43dbf
Nix <lukasgade77@gmail.com>
2021-04-19 11:00:46 +0200

    Trying to figure out firebase system

--- VirtualWorkout/app/src/main/java/com/p2aau2021/virtualworkout/MainActivity.java
+++ VirtualWorkout/app/src/main/java/com/p2aau2021/virtualworkout/MainActivity.java
@@ -1,16 +1,48 @@
 package com.p2aau2021.virtualworkout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("condition");
+
+    TextView mConditionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Yep cock :)
+        mConditionTextView = (TextView) findViewById(R.id.textViewCondition);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String text = snapshot.getValue(String.class);
+                //mCondito
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
     }
 }
