de09cb03edc4f56f380c615295e98bc3d68dc72e
HBendi20 <hbendi20@student.aau.dk>
2021-05-10 16:05:32 +0200

    Update LobbyActivity.java

--- VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/LobbyActivity.java
+++ VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/LobbyActivity.java
@@ -1,9 +1,14 @@
 package com.p2aau.virtualworkoutv2;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.p2aau.virtualworkoutv2.classes.ExerciseProgram;
@@ -14,17 +19,30 @@ public class LobbyActivity extends AppCompatActivity {
     Room room;
     ExerciseProgram exerciseProgram;
     int exerciseInt;
+    private DrawerLayout afDrawerLayout;
+    private ActionBarDrawerToggle afToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
-
+        SetupDrawer();
         //room = new Room();
 
         //GetExtra();
         //ChooseExercise();
     }
+
+    public void SetupDrawer(){
+        afDrawerLayout = (DrawerLayout) findViewById(R.id.mainMenuLayout);
+        afToggle = new ActionBarDrawerToggle(this, afDrawerLayout, R.string.open, R.string.close);
+
+        afDrawerLayout.addDrawerListener(afToggle);
+        afToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
 
     public void onSelectWorkoutClick(View view){
         Intent intent = new Intent(LobbyActivity.this, ChooseWorkoutActivity.class);
@@ -32,8 +50,7 @@ public class LobbyActivity extends AppCompatActivity {
     }
 
     public void onAddFriendToLobbyClick(View view){
-        Intent intent = new Intent(LobbyActivity.this, ChooseWorkoutActivity.class);
-        startActivity(intent);
+        afDrawerLayout.openDrawer(GravityCompat.END);
     }
 
 
