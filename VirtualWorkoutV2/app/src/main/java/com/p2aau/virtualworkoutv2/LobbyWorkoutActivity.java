037b6412773e769b23a4c3d4be835699c0aa4c69
Holm Krestian Leonard Bendixen <72729931+HBendi20@users.noreply.github.com>
2021-05-13 13:51:19 +0200

    Added video to Lobby workout activity

--- VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/LobbyWorkoutActivity.java
+++ VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/LobbyWorkoutActivity.java
@@ -1,7 +1,11 @@
 package com.p2aau.virtualworkoutv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class LobbyWorkoutActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class LobbyWorkoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby_workout);
+
+        VideoView videoView = findViewById(R.id.exercise_video);
+        String videoPath = "android.resource://"+getPackageName()+"/"+R.raw.jumpingjack;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
