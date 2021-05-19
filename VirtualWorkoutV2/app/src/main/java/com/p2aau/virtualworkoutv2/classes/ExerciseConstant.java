bb8078cf6a76106e1a8b4fbd1a1300f5c86b7ee9
MikkelSloth <48725067+MikkelSloth@users.noreply.github.com>
2021-05-19 16:08:31 +0200

    Update ExerciseConstant.java

--- VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/classes/ExerciseConstant.java
+++ VirtualWorkoutV2/app/src/main/java/com/p2aau/virtualworkoutv2/classes/ExerciseConstant.java
@@ -27,75 +27,111 @@ public class ExerciseConstant {
         // -- Cardio -- //
         // - Cardio level 1 - //
         ExerciseProgram c1 = new ExerciseProgram("Cardio Level 1", "A beginner-friendly workout stimulating heart rate");
+<<<<<<< Updated upstream
         c1.addExercise(new Exercise("Jumping Jacks",/*"Jump to a position with legs spread and hands overhead, then jump back to original posture.",*/20000,1,15, R.raw.jumpingjack));
         c1.addExercise(new Exercise("Squats",20000,1,15, R.raw.squats));
+=======
+        c1.addExercise(new Exercise("Jumping Jacks",20000,1,15, R.raw.yumpingyacks));
+        c1.addExercise(new Exercise("Squats",30000,1,15, R.raw.squats));
+>>>>>>> Stashed changes
         c1.addExercise(new Exercise("Knee Push-Ups",20000,1,10, R.raw.pushups));
-        c1.addExercise(new Exercise("Lunges",20000,1,10, R.raw.lunges));
+        c1.addExercise(new Exercise("Lunges",30000,1,10, R.raw.lunges));
 
         EXERCISE_PROGRAMS[0][0]= c1;
 
         // - Cardio level 2
         ExerciseProgram c2 = new ExerciseProgram("Cardio Level 2", "A moderately difficult workout stimulating heart rate");
+<<<<<<< Updated upstream
         c2.addExercise(new Exercise("Run in Place",30000,2,30, R.raw.jumpingjack));
         c2.addExercise(new Exercise("Squats",30000,2,25, R.raw.feetinandoutplank));
         c2.addExercise(new Exercise("Feet in and out",20000,2,20, R.raw.squats));
         c2.addExercise(new Exercise("Push-Ups",20000,2,15, R.raw.pushups));
         c2.addExercise(new Exercise("Big sideways jumps",20000,2,10, R.raw.highknees));
+=======
+        c2.addExercise(new Exercise("Jump jacks",30000,2,30, R.raw.yumpingyacks));
+        c2.addExercise(new Exercise("Feet in and out plank",20000,2,25, R.raw.feetinandoutplank));
+        c2.addExercise(new Exercise("Squats",30000,2,20, R.raw.squats));
+        c2.addExercise(new Exercise("Push-Ups",30000,2,15, R.raw.pushups));
+        c2.addExercise(new Exercise("Run in place",30000,2,10, R.raw.highknees));
+>>>>>>> Stashed changes
 
         EXERCISE_PROGRAMS[0][1]= c2;
 
         // -- Strength -- //
         // - Strength level 1 - //
         ExerciseProgram s1 = new ExerciseProgram("Strength Level 1", "A Basic but diverse workout for increasing physical strength");
-        s1.addExercise(new Exercise("Squats",25000,1,20, R.raw.squats));
-        s1.addExercise(new Exercise("Plank Saw",20000,1,10, R.raw.normalplank));
-        s1.addExercise(new Exercise("High Jumps",15000,1,6, R.raw.tuckjumps));
-        s1.addExercise(new Exercise("Judo Push-Ups",20000,1,10, R.raw.highknees));
+        s1.addExercise(new Exercise("Squats",30000,1,20, R.raw.squats));
+        s1.addExercise(new Exercise("Normal plank",20000,1,10, R.raw.normalplank));
+        s1.addExercise(new Exercise("Tuck jump",20000,1,6, R.raw.tuckjumps));
+        s1.addExercise(new Exercise("Runner touches",20000,1,10, R.raw.runnertouches));
 
         EXERCISE_PROGRAMS[1][0]= s1;
 
         // - Strength level 2 - //
-        ExerciseProgram s2 = new ExerciseProgram("Strength Level 1", "A moderately workout for increasing physical strength");
-        s2.addExercise(new Exercise("Split jumps",20000,1,20, R.raw.squats));
+        ExerciseProgram s2 = new ExerciseProgram("Strength Level 2", "A moderately workout for increasing physical strength");
+        s2.addExercise(new Exercise("Squats",30000,1,20, R.raw.squats));
         s2.addExercise(new Exercise("Explosive Push-Ups",20000,1,6, R.raw.explosivepushups));
-        s2.addExercise(new Exercise("Runner touches",20000,1,16, R.raw.tuckjumps));
-        s2.addExercise(new Exercise("Flying shoulder press",20000,1,10, R.raw.normalplank));
+        s2.addExercise(new Exercise("Tuck jumps",30000,1,16, R.raw.tuckjumps));
+        s2.addExercise(new Exercise("Normal plank",30000,1,10, R.raw.normalplank));
         s2.addExercise(new Exercise("Flying shoulder press",20000,1,10, R.raw.flyingshoulderpress));
 
         EXERCISE_PROGRAMS[1][1]= s2;
+
         // -- Yoga -- //
         // -- Yoga level 1 -- //
-        ExerciseProgram y1 = new ExerciseProgram("Fat Burner Level 1", "An intense workout for burning fat over time");
-        y1.addExercise(new Exercise("Walkouts",60000,1,12, R.raw.lunges));
-        y1.addExercise(new Exercise("Walking Lunges",60000,1,40, R.raw.tuckjumps));
-        y1.addExercise(new Exercise("Jumps",40000,1,60, R.raw.pushups));
-        y1.addExercise(new Exercise("Knee Push-Ups",30000,1,15, R.raw.squats));
-        y1.addExercise(new Exercise("Squats",40000,1,30, R.raw.lowboatcrunches));
-        y1.addExercise(new Exercise("Boat Crunches",30000,1,6, R.raw.highplank));
-
+        ExerciseProgram y1 = new ExerciseProgram("Yoga Level 1", "An intense workout for burning fat over time");
+        y1.addExercise(new Exercise("Child's pose",20000,1,12, R.raw.childspose));
+        y1.addExercise(new Exercise("Cat cow",20000,1,40, R.raw.catcow));
+        y1.addExercise(new Exercise("Down dog pedal feet",20000,1,60, R.raw.peddlefeet));
+        y1.addExercise(new Exercise("Dog down left leg up",10000,1,15, R.raw.downdogleft));
+        y1.addExercise(new Exercise("Dog down right leg up",10000,1,30, R.raw.downdogright));
+        y1.addExercise(new Exercise("High plank",20000,1,6, R.raw.highplank));
+        y1.addExercise(new Exercise("Dog Down",20000,1,6, R.raw.downdog));
+        y1.addExercise(new Exercise("Child's pose",20000,1,6, R.raw.childspose));
         EXERCISE_PROGRAMS[2][0]= y1;
+
+        // -- Yoga level 2 -- //
+        ExerciseProgram y2 = new ExerciseProgram("Yoga level 2", "An intense workout for burning fat over time");
+        y2.addExercise(new Exercise("Child's pose",20000,1,12, R.raw.childspose));
+        y2.addExercise(new Exercise("Cat cow",20000,1,40, R.raw.catcow));
+        y2.addExercise(new Exercise("Down dog",30000,1,60, R.raw.downdog));
+        y2.addExercise(new Exercise("Half lift",20000,1,15, R.raw.halflift));
+        y2.addExercise(new Exercise("High plank",30000,1,30, R.raw.highplank));
+        y2.addExercise(new Exercise("Down dog",20000,1,6, R.raw.downdog));
+        y2.addExercise(new Exercise("Scorpion dog left",10000,1,6, R.raw.scorpiondogleft));
+        y2.addExercise(new Exercise("Scorpion dog right",10000,1,6, R.raw.scorpiondogright));
+        y2.addExercise(new Exercise("Child's pose",20000,1,6, R.raw.childspose));
+        y2.addExercise(new Exercise("Savansa",30000,1,6, R.raw.savansa));
+        EXERCISE_PROGRAMS[2][1]= y2;
 
         // -- Fat burn -- //
         // - Fat burn level 1 - //
         ExerciseProgram f1 = new ExerciseProgram("Fat Burner Level 1", "An intense workout for burning fat over time");
-        f1.addExercise(new Exercise("Walkouts",60000,1,12, R.raw.lunges));
-        f1.addExercise(new Exercise("Walking Lunges",60000,1,40, R.raw.tuckjumps));
-        f1.addExercise(new Exercise("Jumps",40000,1,60, R.raw.pushups));
-        f1.addExercise(new Exercise("Knee Push-Ups",30000,1,15, R.raw.squats));
-        f1.addExercise(new Exercise("Squats",40000,1,30, R.raw.lowboatcrunches));
-        f1.addExercise(new Exercise("Boat Crunches",30000,1,6, R.raw.highplank));
+        f1.addExercise(new Exercise("Lunges",30000,1,12, R.raw.lunges));
+        f1.addExercise(new Exercise("Tuck jumps",20000,1,40, R.raw.tuckjumps));
+        f1.addExercise(new Exercise("Push-ups",20000,1,60, R.raw.pushups));
+        f1.addExercise(new Exercise("Squats",30000,1,15, R.raw.squats));
+        f1.addExercise(new Exercise("Boat to low boat crunches",20000,1,30, R.raw.lowboatcrunches));
+        f1.addExercise(new Exercise("Normal plank",20000,1,6, R.raw.normalplank));
 
 
         EXERCISE_PROGRAMS[3][0]= f1;
 
         // - Fat burn level 2 -- //
         ExerciseProgram f2 = new ExerciseProgram("Fat Burner Level 2", "An intense workout for burning fat over time");
-        f2.addExercise(new Exercise("Alternating Backward Lunges",40000,1,12, R.raw.lunges));
+        f2.addExercise(new Exercise("Lunges",30000,1,12, R.raw.lunges));
         f2.addExercise(new Exercise("High Knees",20000,1,40, R.raw.highknees));
+<<<<<<< Updated upstream
         f2.addExercise(new Exercise("Dolphin Push Ups",40000,1,60, R.raw.pushups));
         f2.addExercise(new Exercise("Lunges to Jumps",20000,1,15, R.raw.squats));
         f2.addExercise(new Exercise("Jump in square",40000,1,30, R.raw.burpee));
         f2.addExercise(new Exercise("2m Sidesteps",20000,1,6, R.raw.normalplank));
+=======
+        f2.addExercise(new Exercise("Push-ups",20000,1,60, R.raw.pushups));
+        f2.addExercise(new Exercise("Squats",30000,1,15, R.raw.squats));
+        f2.addExercise(new Exercise("Boat to low boat crunches",20000,1,30, R.raw.burpees));
+        f2.addExercise(new Exercise("Normal plank",20000,1,6, R.raw.normalplank));
+>>>>>>> Stashed changes
 
         EXERCISE_PROGRAMS[3][1]= f2;
     }
